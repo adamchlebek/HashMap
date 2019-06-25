3e262dce0886f96e077bc6155f73ddd332eb2373
adamchlebek <CHLEBEAD@mail.gvsu.edu>
2019-06-24 22:55:17 -0400

    Created Login and Register Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginAndRegisterComponent } from './login-and-register/login-and-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginAndRegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login-and-register/login-and-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-and-register',
+  templateUrl: './login-and-register.component.html',
+  styleUrls: ['./login-and-register.component.scss']
+})
+export class LoginAndRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
