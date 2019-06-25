bdb736dd4eb8feb02ca17478869aaea0df995316
Larsonco <c.j.larson34@gmail.com>
2019-06-24 23:43:03 -0400

    Angular Routing Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-
+import { BrowserModule }     from '@angular/platform-browser';
+import { NgModule }          from '@angular/core';
+import { AppRoutingModule, routingComponents }  from './app-routing.module';
+import { AppComponent }      from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
