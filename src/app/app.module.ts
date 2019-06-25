4ffbebd8a8532fce5530ff8b969c7a987cbbc4c8
adamchlebek <CHLEBEAD@mail.gvsu.edu>
2019-06-24 23:54:17 -0400

    Readded FA Icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule }     from '@angular/platform-browser';
 import { NgModule }          from '@angular/core';
 import { AppRoutingModule, routingComponents }  from './app-routing.module';
 import { AppComponent }      from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -10,6 +12,7 @@ import { AppComponent }      from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
