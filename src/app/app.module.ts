ed1234e516f50d96965fea5cc39fafec443f07cc
adamchlebek <CHLEBEAD@mail.gvsu.edu>
2019-06-24 23:51:49 -0400

    Merge Conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
-=======
 import { BrowserModule }     from '@angular/platform-browser';
 import { NgModule }          from '@angular/core';
 import { AppRoutingModule, routingComponents }  from './app-routing.module';
 import { AppComponent }      from './app.component';
->>>>>>> bdb736dd4eb8feb02ca17478869aaea0df995316
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,10 +10,6 @@ import { AppComponent }      from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    AngularFontAwesomeModule
-=======
->>>>>>> bdb736dd4eb8feb02ca17478869aaea0df995316
   ],
   providers: [],
   bootstrap: [AppComponent]
