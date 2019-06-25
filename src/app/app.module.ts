44a6fb6f8dad72fdf02c8d80b46197fcc4e77664
adamchlebek <CHLEBEAD@mail.gvsu.edu>
2019-06-24 22:55:27 -0400

    Created New Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginAndRegisterComponent } from './login-and-register/login-and-register.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginAndRegisterComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginAndRegisterComponent } from './login-and-register.component';
+import { RegisterComponent } from './register.component';
 
-describe('LoginAndRegisterComponent', () => {
-  let component: LoginAndRegisterComponent;
-  let fixture: ComponentFixture<LoginAndRegisterComponent>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginAndRegisterComponent ]
+      declarations: [ RegisterComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginAndRegisterComponent);
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
