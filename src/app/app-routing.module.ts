8358ce89773a4fe82fe5c2cd57dda1a2593d08cb
Adam Chlebek <adamchlebek@live.com>
2019-07-15 00:33:58 -0400

    Fixed Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { AboutComponent }         from './about/about.component';
 import { NgModule }               from '@angular/core';
 import { Routes, RouterModule }   from '@angular/router';
-import { LoginComponent }         from './login/login.component';
-import { RegisterComponent }      from './register/register.component';
 import { SetupComponent }         from './setup/setup.component';
 import { AuthGuard }              from './auth.guard';
 import { ReleaseRubricComponent } from './release-rubric/release-rubric.component';
@@ -10,9 +8,7 @@ import { ProfileComponent }       from './profile/profile.component';
 import { NetworkComponent }       from './network/network.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent},
   { path: 'rubric', component: ReleaseRubricComponent},
-  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
   { path: '',
     redirectTo: '/about',
     pathMatch: 'full'
@@ -28,4 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LoginComponent, RegisterComponent, SetupComponent]
+export const routingComponents = [SetupComponent]
