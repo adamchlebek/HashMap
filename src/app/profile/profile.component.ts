2d43a26c7ef1aea2284812c22fa68482e3129426
Adam Chlebek <adamchlebek@live.com>
2019-07-22 15:14:24 -0400

    Fixed Two Coding Standard Errors

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -100,7 +100,7 @@ export class ProfileComponent implements OnInit {
     this.setupAPI.getProfile(this.uid).subscribe((prof: any) => {
       if (prof.exists) {
         this.profile = prof.data();
-        let sa: SteamApp[] = [];
+        const sa: SteamApp[] = [];
         this.profile.steamApps.forEach((appId) => {
           this.api.getSteamApp(appId.toString()).subscribe((app: any) => {
             sa.push(app.data());
